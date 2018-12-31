cf46ff86d2d2b197034ac1e3be3d076b203b6144
EiffL <fr.eiffel@gmail.com>
2018-12-31 03:17:28 -0800

    Adds some plots

--- deepgal/validation/plotting.py
+++ deepgal/validation/plotting.py
@@ -5,6 +5,13 @@ from matplotlib.colors import LogNorm
 import seaborn as sns
 from absl import flags
 
+red = '#A6192E'
+blue= '#002d72'
+yellow = '#F2A900'
+
+sns.set()
+sns.set_style("dark")
+
 def moments_plots(cat, m_real, m_mock, prefix=""):
     """
     Compare moments distributions between real images and mocks
@@ -12,62 +19,13 @@ def moments_plots(cat, m_real, m_mock, prefix=""):
     mask_r = m_real['flag']
     mask_m = m_mock['flag']
     mask = mask_r & mask_m
-    # Add mask to remove objects larger than half the frame
-    mask = mask & (cat['flux_radius'] < 32)
-
-    sns.set_context("paper", font_scale=3.)
-    # Compare the distributions of sizes
-    figure(figsize=(15,5))
-    subplot(121)
-    scatter(cat['flux_radius'][mask], m_real['sigma_e'][mask], c=cat['mag_auto'][mask],cmap='rainbow',vmin=20,alpha=0.6); colorbar(label='Input magnitude')
-    xlim(0,50)
-    ylim(0,20)
-    xlabel("Input half-light radius [pix]")
-    ylabel("Measured moments sigma [pix]")
-    title("COSMOS images")
-    subplot(122)
-    scatter(cat['flux_radius'][mask], m_mock['sigma_e'][mask], c=cat['mag_auto'][mask],cmap='rainbow',vmin=20,alpha=0.6); colorbar(label='Input magnitude')
-    xlim(0,50)
-    ylim(0,20)
-    xlabel("Input half-light radius [pix]")
-    ylabel("Measured moments sigma [pix]")
-    title("Generated images")
-    suptitle("Comparison of Galaxy sizes from moments")
-    savefig(prefix+"radius_vs_sigma.pdf", transparent=True)
-
-    figure(figsize=(7,5))
-    scatter( m_real['sigma_e'][mask], m_mock['sigma_e'][mask], c=cat['flux_radius'][mask],cmap='magma',alpha=0.5, norm=LogNorm(vmin=1, vmax=25))
-    cbar = colorbar(label='Input half-light radius [pix]' )
-    cbar.set_alpha(1); cbar.draw_all()
-    plot([0,20],[0,20],'r',lw=2)
-    xlim(1.5,20)
-    ylim(1.5,20)
-    yscale('log')
-    xscale('log')
-    xlabel("COSMOS size [pix]")
-    ylabel("C-VAE sample size [pix]")
-    #title("Comparison of galaxy sizes measured from moments")
-    savefig(prefix+"size_conditioning.pdf", transparent=True)
-    savefig(prefix+"size_conditioning.png", transparent=True, bbox_inches='tight', pad_inches=0 )
-
-    figure(figsize=(7,5))
-    scatter(m_real['amp'], m_mock['amp'],c=cat['mag_auto'],cmap='magma_r',alpha=0.5,vmax=28,vmin=20)
-    yscale('log')
-    xscale('log')
-    plot([0.5,1000],[0.5,1000],'r',lw=2)
-    xlim(0.5,500)
-    ylim(0.5,500)
-    xlabel("COSMOS brightness")
-    ylabel("C-VAE sample brightness")
-    cbar =colorbar(label='Input magnitude')
-    cbar.set_alpha(1); cbar.draw_all()
-    savefig(prefix+"magnitude_conditioning.pdf", transparent=True)
-    savefig(prefix+"magnitude_conditioning.png", transparent=True, bbox_inches='tight', pad_inches=0 )
+    sns.set()
+    sns.set_style("dark")
 
     figure()
     m = mask
-    g = sns.jointplot(cat['flux_radius'][m], (m_real['sigma_e'][m] - m_mock['sigma_e'][m])/ cat['flux_radius'][m],gridsize=50, kind='hex', xscale='log', bins='log')
-    g.ax_joint.hlines(0,cat['flux_radius'][m].min(),30,color='r')
+    g = sns.jointplot(cat['flux_radius'][m], (m_real['sigma_e'][m] - m_mock['sigma_e'][m])/ cat['flux_radius'][m],gridsize=50, kind='hex', xscale='log', color=blue, bins='log')
+    g.ax_joint.hlines(0,cat['flux_radius'][m].min(),30,color=red)
     g.ax_joint.xaxis.set_label_text('Input half-light radius [pix]');
     g.ax_joint.yaxis.set_label_text('(cosmos - gen) / half_light_radius');
     err_rel = (m_real['sigma_e'][mask] - m_mock['sigma_e'][mask])/ cat['flux_radius'][mask]
@@ -79,24 +37,47 @@ def moments_plots(cat, m_real, m_mock, prefix=""):
     for i in range(n):
         res_m[i] = mean(err_rel[inds == i])
         res_std[i] = std(err_rel[inds == i])
-    g.ax_joint.errorbar(b,res_m, res_std, color='y')
+    g.ax_joint.errorbar(b,res_m, res_std, color=yellow)
     savefig(prefix+"size_error.pdf", transparent=True)
 
-    sns.set_context("paper", font_scale=1.8)
+
+    figure()
+    m = mask & (cat['mag_auto'] > 18 ) & (m_mock['amp'] / m_real['amp'] < 2)
+    g = sns.jointplot(cat['mag_auto'][m],clip(m_mock['amp'][m] / m_real['amp'][m],0.,2),
+                  gridsize=50, kind='hex', color=blue, bins='log',
+                  ylim=(0.,2), xlim=(18,25.2))
+
+    g.ax_joint.hlines(1, 18,25.2, color=red)
+    g.ax_joint.xaxis.set_label_text('mag_auto');
+    g.ax_joint.yaxis.set_label_text('(flux_mock / flux_real)');
+
+    err_rel = (m_mock['amp'][mask] / m_real['amp'][mask])
+    n = 10
+    b = linspace(18,25.2,n)
+    inds = digitize(cat['mag_auto'][mask], b)
+    res_m = zeros(n)
+    res_std = zeros(n)
+    for i in range(n):
+        res_m[i] = mean(err_rel[inds == i])
+        res_std[i] = std(err_rel[inds == i])
+    g.ax_joint.errorbar(b,res_m, res_std, color=yellow)
+    savefig(prefix+"flux_error.pdf", transparent=True)
+
+
     figure(figsize=(5,5))
-    bins = linspace(0,0.8,17)
-    hist(m_real['g'][m], bins,label='COSMOS galaxies',alpha=0.5, normed=True);
-    hist(m_mock['g'][m], bins,label='CVAE samples',alpha=0.5, normed=True);
+    bins = linspace(0,0.8,25)
+    a = hist(m_real['g'][mask], bins,color=red,alpha=0.3, normed=True, label='COSMOS galaxies');
+    b = hist(m_mock['g'][mask], bins,color=blue,alpha=0.3, normed=True, label='mock galaxies');
     xlabel("Ellipticity $|e|$")
     xlim(0,0.8)
     legend()
-    savefig(prefix+"ellipticity.pdf", transparent=True)
+    savefig(prefix+"ellipticity.pdf", transparent=True, bbox_inches='tight', pad_inches=0)
 
     figure(figsize=(5,5))
-    bins = linspace(0,16,17)
-    hist(m_real['sigma_e'][m], bins,label='COSMOS galaxies',alpha=0.5, normed=True);
-    hist(m_mock['sigma_e'][m], bins,label='CVAE samples',alpha=0.5, normed=True);
+    bins = linspace(2.5,16,32)
+    a = hist(m_real['sigma_e'][mask], bins,color=red,alpha=0.3, normed=True, label='COSMOS galaxies');
+    b = hist(m_mock['sigma_e'][mask], bins,color=blue,alpha=0.3, normed=True, label='mock galaxies');
     xlabel("Size $\sigma$ [pix]")
     legend()
     ylim(0,0.20)
-    savefig(prefix+"size.pdf", transparent=True)
+    savefig(prefix+"size.pdf", transparent=True, bbox_inches='tight', pad_inches=0)
